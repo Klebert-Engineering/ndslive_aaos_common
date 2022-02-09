0d5316a79ba964343f88d3333e05839118cfe7c8
Fabian Klebert <f.klebert@klebert-engineering.de>
2022-02-09 20:28:53 +0100

    NDS.Live adaptions

--- additions.mk
+++ additions.mk
@@ -1,12 +1,10 @@
 # Generic overlays
-PRODUCT_PACKAGE_OVERLAYS += device/generic/car/common/overlay device/snappautomotive/common/overlay
+PRODUCT_PACKAGE_OVERLAYS += device/generic/car/common/overlay device/ndslive/common/overlay
 
 # Add non-public overlays if they exist
-$(call inherit-product-if-exists, vendor/snappautomotive/non_public/additions.mk)
+$(call inherit-product-if-exists, vendor/ndslive/non_public/additions.mk)
 
-# Snapp Maps
-PRODUCT_PACKAGES += osmdroid
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
-    device/snappautomotive/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
+    device/ndslive/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
